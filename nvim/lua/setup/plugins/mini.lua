ff9ac09666573863475ffa4ec126aa42a544ee39
杨硕 <yshuo1209@gmail.com>
2025-05-29 14:48:26 +0800

    0529

--- nvim/lua/setup/plugins/mini.lua
+++ nvim/lua/setup/plugins/mini.lua
@@ -1,105 +1,128 @@
 ---@diagnostic disable: undefined-global
 return {
-	{
-		"echasnovski/mini.nvim",
-		version = false,
-		config = function()
-			require("mini.ai").setup()
-			require("mini.pairs").setup()
-			require("mini.surround").setup()
-			require("mini.cursorword").setup()
-			require("mini.bracketed").setup()
-			require("mini.indentscope").setup()
-            require("mini.pick").setup()
-			local hipatterns = require("mini.hipatterns")
-			hipatterns.setup({
-				highlighters = {
-					fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-					hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-					todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-					note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-					hex_color = hipatterns.gen_highlighter.hex_color(),
-				},
-			})
+    {
+        "echasnovski/mini.nvim",
+        version = false,
+        config = function()
+            require("mini.ai").setup()
+            require("mini.pairs").setup()
+            require("mini.surround").setup()
+            require("mini.cursorword").setup()
+            require("mini.bracketed").setup()
+            require("mini.indentscope").setup()
+            require("mini.extra").setup()
 
-			local splitjoin_config = {
-				detect = {
-					separator = "[,;]",
-				},
-			}
-			require("mini.splitjoin").setup(splitjoin_config)
+            local win_config = function()
+                local height = math.floor(0.618 * vim.o.lines)
+                local width = math.floor(0.618 * vim.o.columns)
+                return {
+                    anchor = 'NW',
+                    height = height,
+                    width = width,
+                    row = math.floor(0.5 * (vim.o.lines - height)),
+                    col = math.floor(0.5 * (vim.o.columns - width)),
+                }
+            end
+            local pick_config = {
+                window = { config = win_config() },
+                mappings = {
+                    toggle_info    = '<C-k>',
+                    toggle_preview = '<C-p>',
+                    move_down      = '<Tab>',
+                    move_up        = '<S-Tab>',
+                }
+            }
+            require("mini.pick").setup(pick_config)
 
-			local move_config = {
-				mappings = {
-					-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-					left = "<M-h>",
-					right = "<M-l>",
-					down = "<M-j>",
-					up = "<M-k>",
+            local hipatterns = require("mini.hipatterns")
+            hipatterns.setup({
+                highlighters = {
+                    fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+                    hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+                    todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+                    note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+                    hex_color = hipatterns.gen_highlighter.hex_color(),
+                },
+            })
 
-					-- Move current line in Normal mode
-					line_left = "<M-H>",
-					line_right = "<M-L>",
-					line_down = "<M-J>",
-					line_up = "<M-K>",
-				},
+            local splitjoin_config = {
+                detect = {
+                    separator = "[,;]",
+                },
+            }
+            require("mini.splitjoin").setup(splitjoin_config)
 
-				-- Options which control moving behavior
-				options = {
-					-- Automatically reindent selection during linewise vertical move
-					reindent_linewise = true,
-				},
-			}
-			require("mini.move").setup(move_config)
+            local move_config = {
+                mappings = {
+                    -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+                    left = "<M-h>",
+                    right = "<M-l>",
+                    down = "<M-j>",
+                    up = "<M-k>",
 
-			local files_config = {
-				options = {
-					-- Whether to delete permanently or move into module-specific trash
-					permanent_delete = true,
-					-- Whether to use for editing directories
-					use_as_default_explorer = true,
-				},
+                    -- Move current line in Normal mode
+                    line_left = "<M-H>",
+                    line_right = "<M-L>",
+                    line_down = "<M-J>",
+                    line_up = "<M-K>",
+                },
 
-				-- Customization of explorer windows
-				windows = {
-					-- Maximum number of windows to show side by side
-					max_number = math.huge,
-					-- Whether to show preview of file/directory under cursor
-					preview = false,
-					-- Width of focused window
-					width_focus = 35,
-					-- Width of non-focused window
-					width_nofocus = 15,
-					-- Width of preview window
-					width_preview = 25,
-				},
-			}
-			require("mini.files").setup(files_config)
-			vim.keymap.set("n", "<leader>e", function()
-				if not MiniFiles.close() then
-					MiniFiles.open()
-				end
-			end, { desc = "Open MiniFiles" })
-			local map_split = function(buf_id, lhs, direction)
-				local rhs = function()
-					-- Make new window and set it as target
-					local cur_target = MiniFiles.get_explorer_state().target_window
-					local new_target = vim.api.nvim_win_call(cur_target, function()
-						vim.cmd(direction .. " split")
-						return vim.api.nvim_get_current_win()
-					end)
+                -- Options which control moving behavior
+                options = {
+                    -- Automatically reindent selection during linewise vertical move
+                    reindent_linewise = true,
+                },
+            }
+            require("mini.move").setup(move_config)
 
-					MiniFiles.set_target_window(new_target)
+            local files_config = {
+                options = {
+                    -- Whether to delete permanently or move into module-specific trash
+                    permanent_delete = true,
+                    -- Whether to use for editing directories
+                    use_as_default_explorer = true,
+                },
 
-					-- This intentionally doesn't act on file under cursor in favor of
-					-- explicit "go in" action (`l` / `L`). To immediately open file,
-					-- add appropriate `MiniFiles.go_in()` call instead of this comment.
-				end
+                -- Customization of explorer windows
+                windows = {
+                    -- Maximum number of windows to show side by side
+                    max_number = math.huge,
+                    -- Whether to show preview of file/directory under cursor
+                    preview = false,
+                    -- Width of focused window
+                    width_focus = 35,
+                    -- Width of non-focused window
+                    width_nofocus = 15,
+                    -- Width of preview window
+                    width_preview = 25,
+                },
+            }
+            require("mini.files").setup(files_config)
+            vim.keymap.set("n", "<leader>e", function()
+                if not MiniFiles.close() then
+                    MiniFiles.open()
+                end
+            end, { desc = "Open MiniFiles" })
+            local map_split = function(buf_id, lhs, direction)
+                local rhs = function()
+                    -- Make new window and set it as target
+                    local cur_target = MiniFiles.get_explorer_state().target_window
+                    local new_target = vim.api.nvim_win_call(cur_target, function()
+                        vim.cmd(direction .. " split")
+                        return vim.api.nvim_get_current_win()
+                    end)
 
-				-- Adding `desc` will result into `show_help` entries
-				local desc = "Split " .. direction
-				vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
-			end
+                    MiniFiles.set_target_window(new_target)
+
+                    -- This intentionally doesn't act on file under cursor in favor of
+                    -- explicit "go in" action (`l` / `L`). To immediately open file,
+                    -- add appropriate `MiniFiles.go_in()` call instead of this comment.
+                end
+
+                -- Adding `desc` will result into `show_help` entries
+                local desc = "Split " .. direction
+                vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+            end
 
             vim.api.nvim_create_autocmd("FileType", {
                 pattern = {
@@ -108,57 +131,70 @@ return {
                     "mason",
                     "trouble"
                 },
-                callback = function ()
+                callback = function()
                     vim.b.miniindentscope_disable = true
                 end,
             })
 
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "MiniFilesBufferCreate",
-				callback = function(args)
-					local buf_id = args.data.buf_id
-					-- Tweak keys to your liking
-					map_split(buf_id, "<C-s>", "belowright horizontal")
-					map_split(buf_id, "<C-v>", "belowright vertical")
-					map_split(buf_id, "<C-t>", "tab")
-				end,
-			})
-			-- Custom MiniFileWindow
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "MiniFilesWindowOpen",
-				callback = function(args)
-					local win_id = args.data.win_id
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "MiniFilesBufferCreate",
+                callback = function(args)
+                    local buf_id = args.data.buf_id
+                    -- Tweak keys to your liking
+                    map_split(buf_id, "<C-s>", "belowright horizontal")
+                    map_split(buf_id, "<C-v>", "belowright vertical")
+                    map_split(buf_id, "<C-t>", "tab")
+                end,
+            })
+            -- Custom MiniFileWindow
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "MiniFilesWindowOpen",
+                callback = function(args)
+                    local win_id = args.data.win_id
 
-					-- Customize window-local settings
-					-- vim.wo[win_id].winblend = 50
-					local config = vim.api.nvim_win_get_config(win_id)
-					config.border, config.title_pos = "single", "center"
-					vim.api.nvim_win_set_config(win_id, config)
-				end,
-			})
+                    -- Customize window-local settings
+                    -- vim.wo[win_id].winblend = 50
+                    local config = vim.api.nvim_win_get_config(win_id)
+                    config.border, config.title_pos = "single", "center"
+                    vim.api.nvim_win_set_config(win_id, config)
+                end,
+            })
 
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "MiniFilesWindowUpdate",
-				callback = function(args)
-					local config = vim.api.nvim_win_get_config(args.data.win_id)
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "MiniFilesWindowUpdate",
+                callback = function(args)
+                    local config = vim.api.nvim_win_get_config(args.data.win_id)
 
-					-- Ensure fixed height
-					-- config.height = 10
+                    -- Ensure fixed height
+                    -- config.height = 10
 
-					-- Ensure no title padding
-					local n = #config.title
-					config.title[1][1] = config.title[1][1]:gsub("^ ", "")
-					config.title[n][1] = config.title[n][1]:gsub(" $", "")
+                    -- Ensure no title padding
+                    local n = #config.title
+                    config.title[1][1] = config.title[1][1]:gsub("^ ", "")
+                    config.title[n][1] = config.title[n][1]:gsub(" $", "")
 
-					vim.api.nvim_win_set_config(args.data.win_id, config)
-				end,
-			})
-		end,
-	},
-	{
-		"echasnovski/mini.icons",
-		config = function()
-			require("mini.icons").setup()
-		end,
-	},
+                    vim.api.nvim_win_set_config(args.data.win_id, config)
+                end,
+            })
+        end,
+
+        vim.keymap.set('n', '<leader>ff', '<Cmd>Pick files<Enter>', { noremap = true, silent = true }),
+        vim.keymap.set('n', '<leader>,', '<Cmd>Pick buffers<Enter>', { noremap = true, silent = true }),
+        vim.keymap.set('n', '<leader>fh', '<Cmd>Pick help<Enter>', { noremap = true, silent = true }),
+        vim.keymap.set('n', '<leader>.', function()
+            MiniExtra.pickers.oldfiles({ current_dir = false, preserve_order = false })
+        end, { noremap = true, silent = true }),
+        vim.keymap.set('n', '<leader>ds', function()
+            MiniExtra.pickers.lsp({ scope = 'document_symbol' })
+        end, { noremap = true, silent = true }),
+        vim.keymap.set('n', '<leader>df', function()
+            MiniExtra.pickers.lsp({ scope = 'declaration' })
+        end, { noremap = true, silent = true }),
+    },
+    {
+        "echasnovski/mini.icons",
+        config = function()
+            require("mini.icons").setup()
+        end,
+    },
 }
--- nvim/lua/setup/plugins/snacks.lua
+++ nvim/lua/setup/plugins/snacks.lua
@@ -149,8 +149,8 @@ return
     },
     keys = {
         -- Top Pickers & Explorer
-        { "<leader>.",  function() Snacks.picker.smart() end,                                   desc = "Smart Find Files" },
-        { "<leader>,",  function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
+        -- { "<leader>.",  function() Snacks.picker.smart() end,                                   desc = "Smart Find Files" },
+        -- { "<leader>,",  function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
         { "<leader>/",  function() Snacks.picker.grep() end,                                    desc = "Grep" },
         { "<leader>:",  function() Snacks.picker.command_history() end,                         desc = "Command History" },
         { "<leader>nn", function() Snacks.picker.notifications() end,                           desc = "Notification History" },
@@ -184,7 +184,7 @@ return
         { "<leader>sC", function() Snacks.picker.commands() end,                                desc = "Commands" },
         { "<leader>sd", function() Snacks.picker.diagnostics() end,                             desc = "Diagnostics" },
         { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end,                      desc = "Buffer Diagnostics" },
-        { "<leader>fh", function() Snacks.picker.help() end,                                    desc = "Help Pages" },
+        -- { "<leader>fh", function() Snacks.picker.help() end,                                    desc = "Help Pages" },
         { "<leader>sH", function() Snacks.picker.highlights() end,                              desc = "Highlights" },
         { "<leader>si", function() Snacks.picker.icons() end,                                   desc = "Icons" },
         { "<leader>sj", function() Snacks.picker.jumps() end,                                   desc = "Jumps" },
@@ -203,7 +203,7 @@ return
         { "gr",         function() Snacks.picker.lsp_references() end,                          nowait = true,                     desc = "References" },
         { "gI",         function() Snacks.picker.lsp_implementations() end,                     desc = "Goto Implementation" },
         { "gy",         function() Snacks.picker.lsp_type_definitions() end,                    desc = "Goto T[y]pe Definition" },
-        { "<leader>ss", function() Snacks.picker.lsp_symbols() end,                             desc = "LSP Symbols" },
+        -- { "<leader>ss", function() Snacks.picker.lsp_symbols() end,                             desc = "LSP Symbols" },
         { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end,                   desc = "LSP Workspace Symbols" },
     },
 }
